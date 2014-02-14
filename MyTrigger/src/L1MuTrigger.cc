fd929066847f57df63775e01be16cc80a34dcf85
abdollah110 <smohamadi@gmail.com>
2014-02-14 15:09:40 +0100

    update src

--- MyTrigger/src/L1MuTrigger.cc
+++ MyTrigger/src/L1MuTrigger.cc
@@ -297,13 +297,13 @@ L1MuTrigger::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
     iEvent.getByLabel(L1MuSource_, muonsHandle);
 
 //    Handle <reco::CaloTowersSorted> CaloTowerHandle;
-    Handle <std::vector <const CaloTower*>> CaloTowerHandle;
+    Handle <std::vector <const CaloTower>> CaloTowerHandle;
     iEvent.getByLabel(srcHLTCaloTowers_, CaloTowerHandle);
 
 
     for (vector<l1extra::L1MuonParticle>::const_iterator mu = muonsHandle->begin(); mu != muonsHandle->end(); mu++) {
         cout << "Mu Pt is   " << mu->pt() << endl;
-        for (vector<const CaloTower*>::const_iterator tower = CaloTowerHandle->begin(); tower != CaloTowerHandle->end(); tower++) {
+        for (vector<const CaloTower>::const_iterator tower = CaloTowerHandle->begin(); tower != CaloTowerHandle->end(); tower++) {
             cout << "CaloTower Pt is   " << tower->pt() << endl;
         }
     }
