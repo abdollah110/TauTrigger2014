ad8b18268dca38ff4a83e042633cc7cbed136372
abdollah110 <smohamadi@gmail.com>
2014-02-17 11:38:38 +0100

    update src

--- MyTrigger/src/Efficiency_L1Mu.cc
+++ MyTrigger/src/Efficiency_L1Mu.cc
@@ -202,7 +202,7 @@ Efficiency_L1Mu::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup
     int step1 = 0;
     for (vector<l1extra::L1MuonParticle>::const_iterator mu = muonsHandle->begin(); mu != muonsHandle->end(); mu++) {
         step1++;
-        cout << step1 << "   Mu Pt is   " << mu->pt() << "   Mu eta is   " << mu->eta() << "   Mu Phi is   " << mu->phi() << endl;
+//        cout << step1 << "   Mu Pt is   " << mu->pt() << "   Mu eta is   " << mu->eta() << "   Mu Phi is   " << mu->phi() << endl;
         float isolation02 = 0;
         float isolation03 = 0;
         float isolation04 = 0;
