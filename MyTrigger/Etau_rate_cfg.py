e3ba41df691080f54641a56e5b157f951c32a8fc
abdollah110 <smohamadi@gmail.com>
2014-03-03 12:04:59 +0100

    update src

--- MyTrigger/Etau_rate_cfg.py
+++ MyTrigger/Etau_rate_cfg.py
@@ -6,6 +6,17 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 
 process.maxEvents = cms.untracked.PSet(input=cms.untracked.int32(-1))
 
+process.source = cms.Source( "PoolSource",
+    fileNames = cms.untracked.vstring(
+        'file:file.root',
+    ),
+    secondaryFileNames = cms.untracked.vstring(
+    ),
+    inputCommands = cms.untracked.vstring(
+        'keep *'
+    )
+)
+
 execfile("files.py")
 
 process.demo = cms.EDAnalyzer('Etau_rate'
