8642787ad51ef81aa9599d72fb05f1e05ef6b757
abdollah110 <smohamadi@gmail.com>
2014-03-07 11:41:31 +0100

    update src

--- MyTrigger/src/Efficiency_L1Mu.cc
+++ MyTrigger/src/Efficiency_L1Mu.cc
@@ -211,7 +211,7 @@ Efficiency_L1Mu::analyze(const edm::Event& iEvent) {
     ////////////////////////////////////////////////////////////////////////////////
     //  For efficiency measurement
     ////////////////////////////////////////////////////////////////////////////////
-    for (<pat::TauCollection>::const_iterator ipftau = pftausHandle->begin(); ipftau != pftausHandle->end(); ipftau++) {
+    for (pat::TauCollection::const_iterator ipftau = pftausHandle->begin(); ipftau != pftausHandle->end(); ipftau++) {
         if (ipftau->pt() > 20 && fabs(ipftau->eta()) < 2.3 && ipftau->tauID("decayModeFinding") > 0.5 && ipftau->tauID("byCombinedIsolationDeltaBetaCorrRaw3Hits") > 0.5 && ipftau->tauID("againstMuonTight") > 0.5 && matchToGenTau(ipftau->eta(), ipftau->phi(), iEvent)) {
             offLineTauEff->Fill(ipftau->pt());
             offLineTau->Fill(ipftau->pt());
