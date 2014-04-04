c43cb33fcf7931fa48cf3bcd9783762e50c781e1
abdollah110 <smohamadi@gmail.com>
2014-04-04 12:10:36 +0200

    update src

--- MyTrigger/Etau_rate_cfg.py
+++ MyTrigger/Etau_rate_cfg.py
@@ -3,6 +3,7 @@ import FWCore.ParameterSet.Config as cms
 process = cms.Process("Demo")
 
 process.load("FWCore.MessageService.MessageLogger_cfi")
+process.load("TauTrigger2014.MyTrigger.PFRecoTauDiscriminationAgainstElectron2_cfi")
 
 process.maxEvents = cms.untracked.PSet(input=cms.untracked.int32(-1))
 
