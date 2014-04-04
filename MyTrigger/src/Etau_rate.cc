3972a0ac81483a42cd2485328e7572aefcf67381
abdollah110 <smohamadi@gmail.com>
2014-04-04 12:31:42 +0200

    update src

--- MyTrigger/src/Etau_rate.cc
+++ MyTrigger/src/Etau_rate.cc
@@ -364,10 +364,11 @@ Etau_rate::analyze(const edm::Event& iEvent, const edm::EventSetup& iSetup) {
         bool hasOverlapEle = hasNoOverLapETau(itau->eta(), itau->phi(), iEvent);
         bool discByDecayModeFinding = (itau->tauID("decayModeFinding") > 0.5 ? true : false);
         bool discByIsolation5hits = (itau->tauID("byTrkIsolation5hits") < 3.0 ? true : false);
-//        bool discByNewAntuEle2 = (itau->tauID("byTrkIsolation5hits") < 3.0 ? true : false);
+        cout << "itau->PFRecoTauDiscriminationAgainstElectron2 = " << end;
+        cout << itau->PFRecoTauDiscriminationAgainstElectron2() << end;
         //        bool discByEleLoose = matchToOfflineTausForEleVeto(itau->eta(), itau->phi(), iEvent);
         bool discByEleLoose = 1;
-//        bool discByIsolation = (itau->tauID("byIsolation") > 0.5 ? true : false);
+        //        bool discByIsolation = (itau->tauID("byIsolation") > 0.5 ? true : false);
         //        bool discByIsolation = (itau->tauID("byTrkIsolation") < 3.0 ? true : false);
 
 
