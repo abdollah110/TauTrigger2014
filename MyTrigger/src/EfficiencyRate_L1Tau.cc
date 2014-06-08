bde5240698cf9c4c34d974e0a451f1267265f1ed
abdollah110 <smohamadi@gmail.com>
2014-06-08 10:28:32 +0200

    update src

--- MyTrigger/src/EfficiencyRate_L1Tau.cc
+++ MyTrigger/src/EfficiencyRate_L1Tau.cc
@@ -313,7 +313,8 @@ void EfficiencyRate_L1Tau::analyze(const edm::Event& iEvent, const edm::EventSet
                 numGoodRecoTau++;
             }
         }
-        if (numGoodRecoTau > 1) {
+//        if (numGoodRecoTau > 1) {
+        if (numGoodRecoTau > 0) { //for debugging
             for (pat::TauCollection::const_iterator ipftau = pftausHandle->begin(); ipftau != pftausHandle->end(); ipftau++) {
                 //                if (ipftau->pt() > 20 && fabs(ipftau->eta()) < 2.3 && ipftau->tauID("decayModeFinding") > 0.5 && ipftau->tauID("byMediumCombinedIsolationDeltaBetaCorr3Hits") > 0.5 && ipftau->tauID("againstMuonLoose") > 0.5 && matchToGenTau(ipftau->eta(), ipftau->phi(), iEvent)) {
                 if (ipftau->pt() > 20 && fabs(ipftau->eta()) < 2.3 && ipftau->tauID("decayModeFinding") > 0.5 && ipftau->tauID("byMediumCombinedIsolationDeltaBetaCorr3Hits") > 0.5 && matchToGenTau(ipftau->eta(), ipftau->phi(), iEvent)) {
