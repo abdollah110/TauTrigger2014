af12a97bdf75052df1ce0d392c0501ba86af1a9a
abdollah110 <smohamadi@gmail.com>
2014-09-08 16:37:33 +0200

    update src

--- MyTrigger/src/EfficiencyRate_L1Tau4x8.cc
+++ MyTrigger/src/EfficiencyRate_L1Tau4x8.cc
@@ -480,7 +480,7 @@ void EfficiencyRate_L1Tau4x8::analyze(const edm::Event& iEvent, const edm::Event
             float tauEt = uctIsotau->getFloat("associatedRegionEt", -4);
             cout << "tau Veto bit for ucttau is = " << uctIsotau->getInt("associated4x4Tau", -4) << endl;
             float tauIso = (jetEt - tauEt) / tauEt;
-            int tauIsoVetoBit = uctIsotau->getInt("associated4x4Tau", -4);
+            int tauIsoVetoBit = uctIsotau->getInt("associated4x4Tau", -4) > 0;
 
             UCTTauNoEtaIso.push_back(uctIsotau->pt());
             if (fabs(uctIsotau->eta()) < 2.17) UCTTauIso.push_back(uctIsotau->pt());
